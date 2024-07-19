2a1a4039ce1dced2f64ebb32f821e2e8431ac82c
Kendrick Victor <sunflowerroses801@gmail.com>
2024-07-19 15:16:02 -0700

    Add files via upload

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Jobfinder.io",
+  description: "A Jobfinder web app",
+};
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
