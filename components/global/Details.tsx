fb9e36c6a3f9652f1877731c0088372601b90be7
kendrickweb94 <kendrickzgeorge@gmail.com>
2024-07-31 04:16:47 -0700

    fixed font problems

--- components/global/Details.tsx
+++ components/global/Details.tsx
@@ -9,23 +9,23 @@ const DetailsDemo: React.FC<{ links: Details[] }> = ({ links }) => {
         {links.map((link) => (
           <div
             key={link.id}
-            className=" rounded-2 bg-white  shadow-lg p-3 h-auto ds:w-full lg:w-[240px]"
+            className=" rounded-2 bg-white  shadow-lg p-2 h-auto ds:w-full lg:w-[240px]"
           >
-            <div className="flex flex-row items-center justify-between  gap-3">
-              <div className="p-3 rounded-3 text-white flex items-center">
-                <div className=" w-14 h-14 overflow-hidden object-cover">
+            <div className="flex flex-row items-center justify-between  gap-2">
+              <div className="p-2 rounded-2 text-white flex items-center">
+                <div className=" w-12 h-12 overflow-hidden object-cover">
                   <Image
                     src={link.icon}
                     alt=""
-                    className=" w-16 h-16"
-                    width={15}
-                    height={15}
+                    className=" w-12 h-12"
+                    width={13}
+                    height={13}
                   />
                 </div>
               </div>
               <div className="space-y-2 pr-4">
-                <p className="text-lg font-semibold text-gray-800">{link.data}</p>
-                <p className="text-base text-gray-500">{link.name}</p>
+                <p className="text-base font-semibold text-gray-800">{link.data}</p>
+                <p className="text-sm text-gray-500">{link.name}</p>
               </div>
             </div>
           </div>
