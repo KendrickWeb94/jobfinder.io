af4375aa4b29c229a724f67c6ad28707bc0663a7
kendrickweb94 <kendrickzgeorge@gmail.com>
2024-08-16 15:35:07 -0700

    cons

--- components/global/CanOrEmp.tsx
+++ components/global/CanOrEmp.tsx
@@ -7,8 +7,8 @@ const CanOrEmp = () => {
   return (
     <div className=' w-full mx-auto min-h-[60vh] py-10 max-w-[82%]'>
         <div className="w-full flex gap-5 flex-wrap h-full">
-            <Image src={candidate} alt='' className='  object-cover' />
-            <Image src={employer} alt='' className='  object-cover' />
+            <Image src={candidate} alt='' className='max-w-lg  object-cover' />
+            <Image src={employer} alt='' className=' max-w-lg object-cover' />
         </div>
     </div>
   )
