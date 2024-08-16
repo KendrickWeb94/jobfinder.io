d132934ff7b9f712d820e1f60def140a7e64760f
kendrickweb94 <kendrickzgeorge@gmail.com>
2024-08-16 15:48:42 -0700

    fixed font

--- components/global/CanOrEmp.tsx
+++ components/global/CanOrEmp.tsx
@@ -6,9 +6,9 @@ import React from 'react'
 const CanOrEmp = () => {
   return (
     <div className=' w-full mx-auto min-h-[60vh] py-10 max-w-[82%]'>
-        <div className="w-full flex gap-5 flex-wrap h-full">
-            <Image src={candidate} alt='' className='max-w-lg  object-cover' />
-            <Image src={employer} alt='' className=' max-w-lg object-cover' />
+        <div className="w-full flex gap-5 items-center justify-center flex-wrap h-full">
+            <Image src={candidate} alt='' className='max-w-md  object-cover' />
+            <Image src={employer} alt='' className=' max-w-md object-cover' />
         </div>
     </div>
   )
